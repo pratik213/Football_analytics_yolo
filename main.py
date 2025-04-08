3e19fe4e58c28ce9522c665eb94c61f39cac4381
pratik213 <dahalp30@gmail.com>
2025-04-09 02:44:10 +1000

    Use trackers for supervision

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from utils import read_video,save_video
+from trackers import tracker
 
 def main():
     # Read Video
     video_frames=read_video('input_video/footballclip.mp4')
+
+    # Save Video
+    save_video(video_frames,'output_videos/output_video.avi')
 
 if __name__=="__main__":
     main()
