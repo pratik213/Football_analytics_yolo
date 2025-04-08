603e23813f799a11b12d5a1a9fd7b59a5aa7059d
pratik213 <dahalp30@gmail.com>
2025-04-08 22:49:56 +1000

    reading video using cv2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from utils import read_video,save_video
+
 def main():
-    print("hello world")
+    # Read Video
+    video_frames=read_video('input_video/footballclip.mp4')
 
 if __name__=="__main__":
     main()
